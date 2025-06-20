52daa2eadd1b77001cf7e7a81f772a9ae7ddd91d
mihirpatel999 <mihirpatel9991@gmail.com>
2025-06-20 18:34:39 +0800

    Modified components and logic in Truck_load folder

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -676,7 +676,7 @@ function Navbar() {
 
   const roleAccess = {
     owner: ['plantmaster', 'usermaster', 'truck', 'gate', 'loader', 'reports'],
-    admin: ['plantmaster', 'usermaster', 'gate', 'reports'],
+    admin: ['plantmaster', 'usermaster','truck', 'gate', 'loader','reports'],
     dispatch: ['truck'],
     gatekeeper: ['gate'],
     report: ['reports'],
