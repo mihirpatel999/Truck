23cbf4f79b7fe536b37358d1cbc3a5b11b1ca93f
mihirpatel999 <mihirpatel9991@gmail.com>
2025-06-25 07:11:10 +0000

    e

--- src/GateKeeper.jsx
+++ src/GateKeeper.jsx
@@ -4250,7 +4250,7 @@ function GateKeeper() {
             {/* Bar Chart */}
             <div
               // className="absolute bottom-[60px] left-[50px] h-[75px] flex items-end gap-[2px] z-10"
-               className="absolute bottom-[40px] left-[50px] h-[75px] flex items-end gap-[2px] z-10"
+               className="absolute bottom-[52px] left-[50px] h-[75px] flex items-end gap-[2px] z-10"
               style={{ width: 'calc(100% - 170px)', maxWidth: '370px' }}
             >
               {quantityPanels.map((panel, index) => {
